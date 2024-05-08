886396863644742cf0dddb01740f6e42ed03c646
sahaku0304 <oikawadaisuke.tohtech@gmail.com>
2024-05-08 11:13:16 +0900

    refact reset

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>test test test</p>
+    </div>
+  );
+};
+export default HomePage;
